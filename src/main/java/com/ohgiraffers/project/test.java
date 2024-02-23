cff60f266fd4f3a8f9302f0a46901c43ffb77178
doa <leedoa0819@gmail.com>
2024-02-23 17:16:14 +0900

    자료수정2

--- src/main/java/com/ohgiraffers/project/test.java
+++ src/main/java/com/ohgiraffers/project/test.java
@@ -7,10 +7,18 @@ public class test {
 
         int num = 1;
 
-    }
+
 
 
         char ch = '바';
+
+
+    String name = "조";
+
+    int num3 = 35555;
+    for (int i =0; i < num3; i++ ){
+        System.out.println("i = " + i);
     }
 
+}
 }
