a9b968624db5a2271607a28a1b8584f127c452eb
choseungyou <tmddbs9313@naver.com>
2024-02-29 17:00:37 +0900

    [[[]]]]

--- src/main/java/com/ohgiraffers/project/test.java
+++ src/main/java/com/ohgiraffers/project/test.java
@@ -8,23 +8,16 @@ public class test {
         int num = 1;
 
 
-
-=======
-
         System.out.println("브랜치 수정 후 올리는 중");
 
     }
 
 
-        char ch = '바';
+    char ch = '바';
 
 
     String name = "조";
 
-    int num3 = 35555;
-    for (int i =0; i < num3; i++ ){
-        System.out.println("i = " + i);
-    }
 
-}///
 }
+///////조승윤 주석처리
