1614f173979fe6eb9704d4c193ad1b49e59e9efb
choseungyou <tmddbs9313@naver.com>
2024-03-03 13:20:36 +0900

    [plus 다시 seungyoun]

--- src/main/java/com/ohgiraffers/project/test.java
+++ src/main/java/com/ohgiraffers/project/test.java
@@ -6,30 +6,31 @@ public class test {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("첫번째 정수를 입력해주세요 : ");
-        int first = sc.nextInt();
-        System.out.print("두번째 정수를 입력해주세요 : ");
-        int second = sc.nextInt();
-        System.out.print("연산 기호를 입력해주세요 : ");
-        char op = sc.next().charAt(0);
+        System.out.print("첫 번째 숫자를 입력해주세요:");
+        int num1 = scanner.nextInt();
+
+        System.out.print("두 번째 숫자를 입력해주세요:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("연산 기호를 선택해주세요 (+, -, *, /):");
+        char operator = scanner.next().charAt(0);
 
         int result = 0;
-        switch (op){
-            case '/' :
-                result = first / second;
-                if(second == 0){
-                    System.out.println("0으로 나눌수 없습니다.");
-                }
+
+        switch (operator) {
+
+            case '+':
+                result = num1 + num2;
                 break;
-            case '%' :
-                result = first % second;
-                break;
+
+            default:
+                System.out.println("잘못된 연산 기호입니다...");
+                return;
         }
 
-        System.out.println(first + " " + op + " " + second + " = " + result);
-
+        System.out.println("결과: " + result);
 
     }
 }
