f24af7fff607d13b67a5b5524ba78aaaa2f12599
1eewookon <wookon@naver.com>
2024-03-03 13:30:44 +0900

    [곱하기 기능 2]

---
+++ src/main/java/com/ohgiraffers/calculation/test.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.calculation;
+
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 정수를 입력해주세요 : ");
+        int first = sc.nextInt();
+        System.out.print("두 번째 정수를 입력해주세요 : ");
+        int second = sc.nextInt();
+        System.out.print("연산 기호 입력(+, -, *, /, %) : ");
+        char op = sc.next().charAt(0);
+
+        int result = 0;
+
+        switch (op) {
+
+            case '*' :
+                result = first * second;
+                break;
+
+            default:
+                System.out.println("잘못된 연산기호 입니다.");
+                return;
+        }
+
+        System.out.println(first + " " + op + " " + second + " = " + result);
+
+    }
+
+}
