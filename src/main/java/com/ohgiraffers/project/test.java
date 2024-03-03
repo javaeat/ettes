a4fd1489d161da1e2e2beac65ad50230e34d0177
LEEDOA <leedoa0819@gmail.com>
2024-03-03 13:03:06 +0900

    [수정2]

--- src/main/java/com/ohgiraffers/project/test.java
+++ src/main/java/com/ohgiraffers/project/test.java
@@ -6,24 +6,32 @@ public class test {
 
     public static void main(String[] args) {
 
-
-        int num = 1;
-
-
-        System.out.println("브랜치 수정 후 올리는 중");
         Scanner sc = new Scanner(System.in);
-        String word = sc.nextLine();
+        System.out.print("첫번째 정수를 입력해주세요 : ");
+        int first = sc.nextInt();
+        System.out.print("두번째 정수를 입력해주세요 : ");
+        int second = sc.nextInt();
+        System.out.print("연산 기호를 입력해주세요 : ");
+        char op = sc.next().charAt(0);
+
+        int result = 0;
+        switch (op) {
+            case '/':
+                if (second != 0) {
+                    result = first / second;
+
+                }else {
+                    throw new IllegalArgumentException("0으로 나눌 수 없음");
+                }
+                return;
+
+            case '%':
+                result = first % second;
+                break;
+        }
+
+        System.out.println(first + " " + op + " " + second + " = " + result);
     }
-
-
-    char ch = '바';
-
-
-    String name = "조";
-
-
 }
-///////조승윤 주석처리/////
 
-////박준혁
-// 이도아 입니다였다가 조승윤이었습니다
+
