1d75236749f0c5316f1b9d49fc89d7ff5842ff75
1eewookon <wookon@naver.com>
2024-03-03 22:43:43 +0900

    [곱하기 기능 3]

--- src/main/java/com/ohgiraffers/project/test.java
+++ src/main/java/com/ohgiraffers/project/test.java
@@ -6,52 +6,30 @@ public class test {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int num2 = scanner.nextInt();
-
-        System.out.print("사칙 연산을 선택하세요 (+, -, *, /,%): ");
-        char operator = scanner.next().charAt(0);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 정수를 입력해주세요 : ");
+        int first = sc.nextInt();
+        System.out.print("두 번째 정수를 입력해주세요 : ");
+        int second = sc.nextInt();
+        System.out.print("연산 기호 입력(+, -, *, /, %) : ");
+        char op = sc.next().charAt(0);
 
         int result = 0;
-        switch(operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
+
+        switch (op) {
+
+            case '*' :
+                result = first * second;
                 break;
 
-            case '%':
-                result = num1 % num2;
-                break;
-
-
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else{ throw new IllegalArgumentException("0으로 나눌 수 없음");
-                }
-                break;
             default:
-                System.out.println("잘못된 연산자입니다.");
+                System.out.println("잘못된 연산기호 입니다.");
                 return;
         }
 
-        System.out.println("결과: " + result);
+        System.out.println(first + " " + op + " " + second + " = " + result);
+
     }
+
 }
-
-
-
-
-
-
 
